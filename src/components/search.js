d6c1e61bf601082cac67c72c1eb1f3af3bc154f7
Brian Kim <yrkim98@uw.edu>
2019-10-29 11:45:42 -0700

    added search, signin

--- src/components/search.js
+++ src/components/search.js
@@ -1,13 +1,35 @@
 import React, { Component }  from 'react';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchResults: []
+    }
+    this.state.results = this.getResults();
+
+
+  }
+  // Pre: searchterm is provided to search groups with
+  // Post: groups with all relevant matching names are returned.
+  getResults(searchTerm) {
+    groupMatch = [];
+    //filter by searchterm
+    return groupMatch
+  }
+
+  // Pre: there are groups to show
+  // Post: results are populated into the DOM
+  populateResults(groupMatch){
+
+  }
   render() {
     return(
       <div>
-        Search bar content goes here
+        show search results cards
       </div>
     );
-  }
+    }
 }
 
 export default Search;
--- src/components/signin.js
+++ src/components/signin.js
@@ -1,10 +1,28 @@
 import React, { Component }  from 'react';
 
 class Signin extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+      userID: 0,
+      userName: ""
+    }
+  }
+
+  // Pre: Username and password provided from user
+  // Post: Authenticates username and password, returns true if sucessful login,
+  // otherwise returns false
+  signIn(providedUserName, providedPass) {
+    //firebase signin authentication
+    return true
+  }
+
+  
   render() {
     return(
       <div>
-        Sign-in content goes here
+        Sign-in form
       </div>
     );
   }
