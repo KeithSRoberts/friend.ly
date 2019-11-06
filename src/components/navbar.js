0fc629c9fa41b76a33f4e00326098c7b184de680
Kyler Griffith <lockheedmartian@gmail.com>
2019-11-05 21:45:46 -0800

    factored syntax

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,27 +9,30 @@ class Navbar extends Component {
   render() {
     return(
       <div className="navigation-bar">
-        <div className="nav-group"></div>
+        <div className="nav-group" />
         <div className="nav-group-main">
           <div className="logo-group nav-group">
             <Link to={routes.SPLASH}>
-              <div className="logo"></div>
+              <div className="logo" />
             </Link>
             <h1 className="nav-title"><span className="nav-friend">Friend</span><span className="nav-ly">.ly</span></h1>
           </div>
-          <div className="nav-group-inner-main"></div>
+          <div className="nav-group-inner-main" />
           <div className="icon-group nav-group">
             <Link to={routes.GROUPS}>
-              <div className="home icon"></div>
+              <div className="home icon" />
             </Link>
-            <div className="search icon"></div>
+            <div className="search icon" />
             <Link to={routes.CREATE_GROUP}>
-              <div className="create icon"></div>
+              <div className="create icon" />
             </Link>
-            <div className="account icon"></div>
+            <Link to={routes.ACCOUNT + '/123'}>
+              <div className="account icon" />
+            </Link>
+            
           </div>
         </div>
-        <div className="nav-group"></div>
+        <div className="nav-group" />
       </div>
     );
   }
