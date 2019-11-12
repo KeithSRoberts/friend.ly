b580152accec5e3b57766d8e7c05410151ea0cb5
anuraagpolisetty <anuraag@uw.edu>
2019-11-12 01:02:26 -0800

    finished Create post modal

--- src/components/createPost.js
+++ src/components/createPost.js
@@ -1,29 +1,66 @@
 import React, { Component }  from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-class CreatePost extends Component {
-  constructor() {
-    super();
+// class CreatePost extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       title: "",
+//       content: "",
+//       links: ""
+//     }
+//   }
+
+//   // Pre: User is signed in and member of group
+//   // Post: Updates new post in firebase database
+//   createNewPost() {
+//   }
+
+
+//   render() {
+//     return(
+//       <div>
+//         Create Post content goes here
+//       </div>
+//     );
+//   }
+
+// }
+
+// export default CreatePost;
+class createPost extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      title: "",
-      content: "",
-      links: ""
-    }
+      modal: true
+    };
+
+    this.toggle = this.toggle.bind(this);
   }
 
-  // Pre: User is signed in and member of group
-  // Post: Updates new post in firebase database
-  createNewPost() {
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
   }
-
 
   render() {
-    return(
+    return (
       <div>
-        Create Post content goes here
+        {/* <Button id="create-post-button" onClick={this.toggle}>New Post</Button>
+            <Modal isOpen={this.state.modal} toggle={this.toggle}>
+              <ModalHeader>Create a post</ModalHeader>
+              <ModalBody>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </ModalBody>
+              <ModalFooter>
+                <Button onClick={this.toggle}>Submit Post</Button>{' '}
+                <Button onClick={this.toggle}>Cancel</Button>
+              </ModalFooter>
+            </Modal> */}
       </div>
     );
   }
-
 }
 
-export default CreatePost;
+export default <createPost buttonLabel='open modal' />;
--- src/components/discussionBoard.js
+++ src/components/discussionBoard.js
@@ -1,6 +1,7 @@
 import React, { Component }  from "react";
-import { Button } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import "./css/discussionBoard.css";
+import createPost from "./createPost";
 
 // import * as routes from '../constants/routes';
 
@@ -8,9 +9,11 @@ class discussionBoard extends Component {
   constructor() {
     super();
     this.state = {
-      posts: []
+      posts: [],
+      modal: false
     }
     this.state.posts = this.fetchPosts();
+    this.toggle = this.toggle.bind(this);
   }
 
   // componentDidMount() {
@@ -23,47 +26,51 @@ class discussionBoard extends Component {
   fetchPosts() {
     let posts = [];
     let testPost = {
-      author: "Member Name",
-      upvotes: 0,
+      author: "Bojack",
+      upvotes: 9,
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id nisi placerat, luctus nisi ac, semper tellus. Aenean tristique auctor quam    ,     vitae accumsan enim posuere sit amet.",
       // image: "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/vcta1kcxdr5y5xyulxel.png"
       // image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/short-haired-dogs-boston-terrier-1563206936.jpg?crop=0.668xw:1.00xh;0.167xw,0&resize=640:*"
       image: "https://t2conline.com/wp-content/uploads/2017/12/vizslasf4.jpg"
     }
     posts.push(testPost);
+    posts.push(testPost);
     return posts;
   }
 
-  createPosts() {
+  renderPosts() {
     let posts = this.state.posts.map((p) => {
       return(
         <div className="post-container">
           <div className="post-buttons">
-            <Button className="button-upvote">
-              <img src="../../img/button-upvote.png"/>
-              </Button>
-            {p.upvotes}
-            <Button className="button-downvote">
-              <img src="../../img/button-downvote"/>
-              </Button>  
+            <Button className="button-upvote"
+              />
+            <h3>{p.upvotes}</h3>
+            <Button className="button-downvote"/>
           </div>
           <div className="post-content">
             <div className="post-author-info">
               <div className="post-image">
-                <img src={p.image} />
+                <img src={p.image} alt={p.author}/>
               </div>
               <div className="post-author">
-                {p.author}
+                <h5>{p.author}</h5>
               </div>
             </div>
             <div className="post-text">
-              {p.description}
+              <p>{p.description}</p>
             </div>
           </div>
         </div>
       )
     })
     return posts;
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
   }
 
 
@@ -77,15 +84,24 @@ class discussionBoard extends Component {
  
     return(
       <div>
-        <Button id="create-post-button">New Post</Button>
-        {/* <Modal onClick={toggle}>
-          <ModalBody>
-            Make a post
-          </ModalBody>
-        </Modal> */}
         <div className="discussion-body">
+          <div className="create-post-button-area">
+            <Button id="create-post-button" onClick={this.toggle}>New Post</Button>
+            <Modal isOpen={this.state.modal} toggle={this.toggle}>
+              <ModalHeader>Create a post</ModalHeader>
+              <ModalBody>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </ModalBody>
+              <ModalFooter>
+                <div className="modal-buttons">
+                  <Button onClick={this.toggle}>Submit Post</Button>
+                  <Button onClick={this.toggle}>Cancel</Button>
+                </div>
+              </ModalFooter>
+            </Modal>
+          </div>
           <div className="discussion-content">
-            {this.createPosts()}
+            {this.renderPosts()}
           </div>
         </div>
       </div>        
