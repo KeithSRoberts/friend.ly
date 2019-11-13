8a12b241718156c66457bb3cd84ce7545cef35c0
Brian Kim <yrkim98@uw.edu>
2019-11-12 16:30:25 -0800

    navbar looks way better now

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,30 +12,32 @@ class Navbar extends Component {
     return(
       // Navbar
       <div className="navigation-bar">
-        <div className="logo-group nav-group nav-icon">
-             <Link to={routes.SPLASH}>
-               <div className="logo" />
-              </Link>
-             <h1 className="nav-title"><span className="nav-friend">Friend</span><span className="nav-ly">.ly</span></h1>
-           </div>
-        {/* Pills */}
-        <Nav pills className="navbar navbar-expand-lg bg-light nav-buttons d-flex justify-content-center">
-          <NavItem>
-            <NavLink href="dash" active id="nav-pill">Dashboard</NavLink>
-          </NavItem>
-          
-          <NavItem>
-            <NavLink href="#" id="nav-pill">asdf</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#" id="nav-pill">Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink disabled href="#" id="nav-pill">Hello</NavLink>
-          </NavItem>
-        </Nav>
-        <Search id="search-bar">
-        </Search>
+        <div className="navigation-bar-inside d-flex flex-row">
+          <div className="logo-group nav-group nav-icon">
+              <Link to={routes.SPLASH}>
+                <div className="logo" />
+                </Link>
+              <h1 className="nav-title"><span className="nav-friend">Friend</span><span className="nav-ly">.ly</span></h1>
+            </div>
+          {/* Pills */}
+          <Nav pills className="navbar navbar-expand-lg nav-buttons d-flex justify-content-center">
+            <NavItem>
+              <NavLink href="dash" active id="nav-pill">Dashboard</NavLink>
+            </NavItem>
+            
+            <NavItem>
+              <NavLink href="#" id="nav-pill">asdf</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#" id="nav-pill">Link</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink disabled href="#" id="nav-pill">Hello</NavLink>
+            </NavItem>
+          </Nav>
+          <Search id="search-bar">
+          </Search>
+        </div>
     </div>
 
     );
