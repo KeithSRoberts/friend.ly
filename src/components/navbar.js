8a1c0257eb829c3466a7589426390429a29f4a27
leoguo <greenkang1234@gmail.com>
2019-11-24 14:22:46 -0800

    NavBar now reflects the design spec

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,15 +12,15 @@ class Navbar extends Component {
     return(
       // Navbar
       <div className="navigation-bar">
-        <div className="navigation-bar-inside d-flex flex-row">
-          <div className="logo-group nav-group nav-icon">
+        <div className="navigation-bar-inside flex-row">
+          <div className="logo-group">
               <Link to={routes.SPLASH}>
                 <div className="logo" />
-                </Link>
-              <h1 className="nav-title"><span className="nav-friend">Friend</span><span className="nav-ly">.ly</span></h1>
-            </div>
+              </Link>
+            <h1 className="nav-title"><span className="nav-friend">Friend</span><span className="nav-ly">.ly</span></h1>
+          </div>
           {/* Pills */}
-          <Nav pills className="navbar navbar-expand-lg nav-buttons d-flex justify-content-center">
+          {/* <Nav pills className="navbar navbar-expand-lg nav-buttons d-flex justify-content-center">
             <NavItem>
               <Link to={routes.GROUPS} active id="nav-pill">Dashboard</Link>
             </NavItem>
@@ -34,9 +34,57 @@ class Navbar extends Component {
             <NavItem>
               <Link disabled to={routes.SPLASH} id="nav-pill">Hello</Link>
             </NavItem>
-          </Nav>
-          <Search id="search-bar">
-          </Search>
+          </Nav> */}
+          
+          {/* <ul class="nav nav-buttons ">
+            <li class="nav-item nav-pill">
+              <a class="nav-link" href={routes.GROUPS} active id="nav-pill">
+                <div class="home" />
+              </a>
+            </li>
+            
+            <li class="nav-item nav-pill">
+              <a class="nav-link" href={routes.SPLASH} id="nav-pill">
+                <div class="create" />
+              </a>
+            </li>
+            <li class="nav-item nav-pill">
+              <a class="nav-link" href={routes.SPLASH} id="nav-pill">
+                <div class="account" />
+              </a>
+            </li>
+            <li class="nav-item nav-pill">
+              <a class="nav-link" href={routes.SPLASH} id="nav-pill">
+                <div class="search" />
+              </a>
+            </li>
+          </ul> */}
+
+          <div id="icons">
+            <div class="nav nav-buttons ">
+                <a class="nav-item nav-pill nav-link home" href={routes.GROUPS} active id="nav-pill" />
+                <a class="nav-item nav-pill nav-link create" href={routes.CREATE_GROUP} id="nav-pill" />
+                <a class="nav-item nav-pill nav-link account" href={routes.ACCOUNT} id="nav-pill" />
+                <a class="nav-item nav-pill nav-link search" href={routes.VIEW_GROUP} id="nav-pill" />
+            </div>
+          </div>
+
+          {/* <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#">Disabled</a>
+  </li>
+</ul> */}
+          {/* <Search id="search-bar">
+          </Search> */}
         </div>
     </div>
 
@@ -44,7 +92,7 @@ class Navbar extends Component {
   }
 }
 
-{/* Dropdown Item 
+{/* Dropdown Item
 <Dropdown nav isOpen={false} toggle={false}>
             <DropdownToggle nav caret>
               Dropdown
